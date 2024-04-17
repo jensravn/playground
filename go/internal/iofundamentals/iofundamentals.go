db2ddc93918b76960471f38e417882d63542a64b
jensravn <jensravn102@pm.me>
2024-04-17 18:58:28 +0200

    io fundamentals

---
+++ go/internal/iofundamentals/iofundamentals.go
@@ -0,0 +1,32 @@
+package iofundamentals
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+)
+
+func tryFmtPrintF(in []byte) []byte {
+	s := string(in)
+	var buff bytes.Buffer // bytes.Buffer implements io.ReadWriter
+	fmt.Fprintf(&buff, s)
+	out := buff.Bytes()
+	return out
+}
+
+func tryIoCopy(in []byte) []byte {
+	src := bytes.NewReader(in) // bytes.Reader implements io.Reader
+	var dst bytes.Buffer
+	io.Copy(&dst, src)
+	out := dst.Bytes()
+	return out
+}
+
+func tryIoReadAll(in []byte) []byte {
+	r := bytes.NewReader(in)
+	out, err := io.ReadAll(r)
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
---
+++ go/internal/iofundamentals/iofundamentals_test.go
@@ -0,0 +1,33 @@
+package iofundamentals
+
+import (
+	"testing"
+)
+
+// https://medium.com/@andreiboar/fundamentals-of-i-o-in-go-c893d3714deb
+
+func Test_tryFmtPrintF(t *testing.T) {
+	in := []byte("Hello, World!")
+	out := tryFmtPrintF(in)
+	if string(in) != string(out) {
+		t.Errorf("tryIoReadAll() = %v, want %v", out, in)
+	}
+}
+
+func Test_tryIoCopy(t *testing.T) {
+	in := []byte("Hello, World!")
+	out := tryIoCopy(in)
+	if string(in) != string(out) {
+		t.Errorf("tryIoReadAll() = %v, want %v", out, in)
+	}
+}
+
+func Test_tryIoReadAll(t *testing.T) {
+	in := []byte("Hello, World!")
+	out := tryIoReadAll(in)
+	if string(in) != string(out) {
+		t.Errorf("tryIoReadAll() = %v, want %v", out, in)
+	}
+}
+
+// https://medium.com/@andreiboar/fundamentals-of-i-o-in-go-part-2-e7bb68cd5608
