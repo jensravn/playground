77ebdbb0704f99ca70dbae29ede48bf8a1f22915
Jens Ravn Jensen <jensravn102@pm.me>
2024-04-30 21:47:29 +0200

    r - first

---
+++ r/first.r
@@ -0,0 +1,17 @@
+# get beginning of the data set
+head(PlantGrowth)
+
+# summarize the data set
+str(PlantGrowth)
+
+# column 1
+PlantGrowth[, 1]
+
+# row 11 to 20 all columns
+PlantGrowth[11:20, ]
+
+# pick variable weight in the data set PlantGrowth
+with(PlantGrowth, weight)
+
+# pick file
+file.choose()
