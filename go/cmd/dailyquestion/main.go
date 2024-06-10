46925552a829a89c61458bed5e0c13d3549cc992
jensravn <jens.ravn.jensen@ingka.com>
2024-06-10 15:47:07 +0200

    daily question for all exams

--- go/cmd/dailyquestion/main.go
+++ go/cmd/dailyquestion/main.go
@@ -5,12 +5,32 @@ import (
 	"math/rand"
 )
 
+var exams = []struct {
+	Exam      string
+	Questions int
+}{
+	{Exam: "associate-cloud-engineer", Questions: 266},
+	{Exam: "cloud-digital-leader", Questions: 286},
+	{Exam: "professional-cloud-architect", Questions: 276},
+	{Exam: "professional-cloud-database-engineer", Questions: 132},
+	{Exam: "professional-cloud-developer", Questions: 279},
+	{Exam: "professional-cloud-devops-engineer", Questions: 166},
+	{Exam: "professional-cloud-network-engineer", Questions: 172},
+	{Exam: "professional-cloud-security-engineer", Questions: 244},
+	{Exam: "professional-collaboration-engineer", Questions: 79},
+	{Exam: "professional-data-engineer", Questions: 311},
+	{Exam: "professional-google-workspace-administrator", Questions: 132},
+	{Exam: "professional-machine-learning-engineer", Questions: 285},
+}
+
 func main() {
-	n := rand.Intn(195) + 1
-	page := getPage(n)
-	url := "https://www.examtopics.com/exams/google/professional-cloud-architect/view/%d\n"
-	fmt.Printf(url, page)
-	fmt.Printf("Question #%d\n", n)
+	n := rand.Intn(len(exams))
+	e := exams[n]
+	q := rand.Intn(e.Questions) + 1
+	page := getPage(q)
+	url := "https://www.examtopics.com/exams/google/%s/view/%d\n"
+	fmt.Printf(url, e.Exam, page)
+	fmt.Printf("%s #%d\n", e.Exam, q)
 }
 
 func getPage(question int) int {
