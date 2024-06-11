46351eb9bfbb824c437e319b3710a6128d558825
jensravn <jens.ravn.jensen@ingka.com>
2024-06-11 14:07:57 +0200

    daily seed

--- go/cmd/dailyquestion/main.go
+++ go/cmd/dailyquestion/main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"math/rand/v2"
+	"time"
 )
 
 var exams = []struct {
@@ -11,27 +12,30 @@ var exams = []struct {
 	PageSize  int
 }{
 	{Exam: "associate-cloud-engineer", Questions: 266},
-	{Exam: "cloud-digital-leader", Questions: 286},
+	{Exam: "cloud-digital-leader", Questions: 70},
 	{Exam: "professional-cloud-architect", Questions: 276},
-	{Exam: "professional-cloud-database-engineer", Questions: 132},
+	{Exam: "professional-cloud-database-engineer", Questions: 132, PageSize: 4},
 	{Exam: "professional-cloud-developer", Questions: 279},
-	{Exam: "professional-cloud-devops-engineer", Questions: 166},
+	{Exam: "professional-cloud-devops-engineer", Questions: 166, PageSize: 4},
 	{Exam: "professional-cloud-network-engineer", Questions: 172, PageSize: 4},
 	{Exam: "professional-cloud-security-engineer", Questions: 244},
 	{Exam: "professional-collaboration-engineer", Questions: 79},
-	{Exam: "professional-data-engineer", Questions: 311},
-	{Exam: "professional-google-workspace-administrator", Questions: 132},
+	{Exam: "professional-data-engineer", Questions: 311, PageSize: 10},
+	{Exam: "professional-google-workspace-administrator", Questions: 132, PageSize: 4},
 	{Exam: "professional-machine-learning-engineer", Questions: 285},
 }
 
 func main() {
-	n := rand.Intn(len(exams))
+	t := time.Now()
+	seed := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).UnixNano()
+	r := rand.New(rand.NewPCG(uint64(seed), 2))
+	n := r.IntN(len(exams))
 	e := exams[n]
-	q := rand.Intn(e.Questions) + 1
+	q := r.IntN(e.Questions) + 1
 	page := getPage(q, e.PageSize)
 	url := "https://www.examtopics.com/exams/google/%s/view/%d\n"
-	fmt.Printf(url, e.Exam, page)
 	fmt.Printf("%s #%d\n", e.Exam, q)
+	fmt.Printf(url, e.Exam, page)
 }
 
 func getPage(question int, pageSize int) int {
