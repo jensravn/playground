bb2ecd27d714827bc7707d6188507efb93e8305e
Jens Ravn Jensen <jensravn102@pm.me>
2024-10-18 14:45:32 +0200

    feat: dataflow pubsub message

--- go/cmd/dataflow-pubsub-window/main.go
+++ go/cmd/dataflow-pubsub-window/main.go
@@ -11,13 +11,9 @@ import (
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/io/pubsubio"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/log"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/x/beamx"
-	"github.com/jensravn/playground/go/internal/gob"
-	"github.com/jensravn/playground/go/internal/item"
 )
 
-func init() {
-	// register.DoFn1x2[]()
-}
+func init() {}
 
 func main() {
 	flag.Parse()
@@ -25,19 +21,18 @@ func main() {
 	ctx := context.Background()
 	p := beam.NewPipeline()
 	s := p.Root()
-	data := pubsubio.Read(s, "jensravn-playground", "topic-id", &pubsubio.ReadOptions{WithAttributes: true})
-	kvData := beam.ParDo(s, func(message *pubsubpb.PubsubMessage) (string, []byte) {
-		e, err := gob.Decode[item.Entity](message.Data)
-		if err != nil {
-			log.Fatalf(ctx, "Failed to decode entity: %v", err)
-		}
-		key := e.No + e.Type
-		return key, message.Data
-	}, data)
-	windowed := beam.WindowInto(s, window.NewFixedWindows(30*time.Second), kvData)
-	combined := beam.CombinePerKey(s, func(v, _ []byte) []byte { return v }, windowed)
+
+	messages := pubsubio.Read(s, "jensravn-playground", "topic-id", &pubsubio.ReadOptions{WithAttributes: true})
+	kvPairs := beam.ParDo(s, func(m *pubsubpb.PubsubMessage) (string, *pubsubpb.PubsubMessage) {
+		return "test", m
+	}, messages)
+	windowed := beam.WindowInto(s, window.NewFixedWindows(30*time.Second), kvPairs)
+	combined := beam.CombinePerKey(s, func(m, _ *pubsubpb.PubsubMessage) *pubsubpb.PubsubMessage {
+		return m
+	}, windowed)
 	deduplicated := beam.DropKey(s, combined)
 	pubsubio.Write(s, "jensravn-playground", "topic-id-2", deduplicated)
+
 	if err := beamx.Run(ctx, p); err != nil {
 		log.Fatalf(ctx, "Failed to execute job: %v", err)
 	}
