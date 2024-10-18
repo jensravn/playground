4896ebf38a5e30303f47bd7c225da5d555be68b6
Jens Ravn Jensen <jensravn102@pm.me>
2024-10-18 14:43:56 +0200

    pubsubpb.PubsubMessage

--- go/cmd/dataflow-pubsub-window/main.go
+++ go/cmd/dataflow-pubsub-window/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"time"
 
+	"cloud.google.com/go/pubsub/apiv1/pubsubpb"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/core/graph/window"
 	"github.com/apache/beam/sdks/v2/go/pkg/beam/io/pubsubio"
@@ -20,12 +21,18 @@ func main() {
 	ctx := context.Background()
 	p := beam.NewPipeline()
 	s := p.Root()
-	data := pubsubio.Read(s, "jensravn-playground", "topic-id", nil)
-	kvData := beam.ParDo(s, func(elm []byte) ([]byte, []byte) { return []byte("test"), elm }, data)
-	windowed := beam.WindowInto(s, window.NewFixedWindows(30*time.Second), kvData)
-	combined := beam.CombinePerKey(s, func(v, _ []byte) []byte { return v }, windowed)
+
+	messages := pubsubio.Read(s, "jensravn-playground", "topic-id", &pubsubio.ReadOptions{WithAttributes: true})
+	kvPairs := beam.ParDo(s, func(m *pubsubpb.PubsubMessage) (string, *pubsubpb.PubsubMessage) {
+		return "test", m
+	}, messages)
+	windowed := beam.WindowInto(s, window.NewFixedWindows(30*time.Second), kvPairs)
+	combined := beam.CombinePerKey(s, func(m, _ *pubsubpb.PubsubMessage) *pubsubpb.PubsubMessage {
+		return m
+	}, windowed)
 	deduplicated := beam.DropKey(s, combined)
 	pubsubio.Write(s, "jensravn-playground", "topic-id-2", deduplicated)
+
 	if err := beamx.Run(ctx, p); err != nil {
 		log.Fatalf(ctx, "Failed to execute job: %v", err)
 	}
