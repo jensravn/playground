6cbf412043feeb054d6e4c71f2f1a86ed66c7124
Jens Ravn Jensen <jens.ravn.jensen@ingka.com>
2025-08-20 14:24:02 +0200

    go text/template

---
+++ go/cmd/text-template/pets.go
@@ -0,0 +1,50 @@
+// Example from: https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-go
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Pet struct {
+	Name   string
+	Sex    string
+	Intact bool
+	Age    string
+	Breed  string
+}
+
+func main() {
+	dogs := []Pet{
+		{
+			Name:   "Jujube",
+			Sex:    "Female",
+			Intact: false,
+			Age:    "10 months",
+			Breed:  "German Shepherd/Pitbull",
+		},
+		{
+			Name:   "Zephyr",
+			Sex:    "Male",
+			Intact: true,
+			Age:    "13 years, 3 months",
+			Breed:  "German Shepherd/Border Collie",
+		},
+		{
+			Name:   "Bruce Wayne",
+			Sex:    "Male",
+			Intact: false,
+			Age:    "3 years, 8 months",
+			Breed:  "Chihuahua",
+		},
+	}
+	tmplFile := "pets.go.tmpl"
+	tmpl, err := template.New(tmplFile).ParseFiles(tmplFile)
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, dogs)
+	if err != nil {
+		panic(err)
+	}
+}
